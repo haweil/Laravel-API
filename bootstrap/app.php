c4b631592c424047bad22d5f1fd1e4f80d00d30a
haweil <92003311+haweil@users.noreply.github.com>
2024-07-03 18:19:55 +0300

    "Simple-Laravel -API "

---
+++ bootstrap/app.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Mail\AuthorCount;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Application;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+   ->withSchedule(function (Schedule $schedule) {
+       $schedule->call(function () {
+           Mail::to('haweil@admin.com')->send(new AuthorCount());
+       })->everyMinute();
+   })
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
